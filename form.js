30aed7ad23622127ab4736ef8b93f75facc8db54
vmundra <krishnamundra91@gmail.com>
2021-04-12 11:36:55 +0530

    made a post route for owners form

--- form.js
+++ form.js
@@ -1,14 +1,34 @@
 
 var express = require('express');
 var router = express.Router();
-var empModel = require('../models/employee');
+var empModel = require('./models/employee');
 var employee = empModel.find({});
 
-router.get('/form',function(req,res,next){
+router.get("/form",function(req,res,next){
     employee.exec(function(err,data){
         if(err) throw err;
         res.render('form',{title : 'Employee Records', recrods:data});
     });
+});
+
+router.post("/form",function(req,res,next){
+
+    var empDetails = new empModel({
+        name: req.body.uname,
+        email: req.body.email,
+        etype: req.body.emptype
+    });
+
+    console.log(empDetails);
+    empDetails.save(function(err,res1){
+        if(err) throw err;
+        employee.exec(function(err,data){
+            if(err) throw err;
+            res.render('form',{title : 'Employee Records', recrods:data});
+        });
+    });
+
+    
 });
 
 module.exports = router;
